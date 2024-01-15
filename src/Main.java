d4163d5d2eec5302366ecd94b2de6b9b454b5def
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-15 17:45:06 +0300

    Oyuncu kedi methodu tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,14 @@ public class Main {
         System.out.println("Yaş Tespiti: " + hasTeen2(9, 99, 19));
         System.out.println("Yaş Tespiti: " + hasTeen2(23, 15, 42));
         System.out.println("Yaş Tespiti: " + hasTeen2(22, 23, 34));
+
+        System.out.println("Oyuncu Kedi: " + isCatPlaying(true, 10));
+        System.out.println("Oyuncu Kedi: " + isCatPlaying(false, 36));
+        System.out.println("Oyuncu Kedi: " + isCatPlaying(false, 35));
+
+        System.out.println("Oyuncu Kedi: " + isCatPlaying2(true, 10));
+        System.out.println("Oyuncu Kedi: " + isCatPlaying2(false, 36));
+        System.out.println("Oyuncu Kedi: " + isCatPlaying2(false, 35));
     }
 
     // NOT: Metodumuzu statik bir main metodunun icinde cagirabilmek icin statik yazmaliyiz.
@@ -22,11 +30,15 @@ public class Main {
 
     // NOT: Metodumuz true ya da false bir deger döneceği için public static boolean seklinde
     // yazdik!
+
+    //HAVLAYAN KOPEGIMIZ COZUM:
     public static boolean shouldWakeUp(boolean isDogBarking, int clock){
 
         if(clock < 0 || clock > 23 || !isDogBarking) return false;
         return clock < 8 || clock >= 20;
     }
+
+    //YAS TESPITI COZUM:
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge){
         return (firstAge >= 13 && firstAge <=19) || (secondAge >= 13 && secondAge <=19) || (thirdAge >= 13 && thirdAge <=19);
@@ -44,5 +56,21 @@ public class Main {
             }
         }
         return false;
+    }
+
+    //OYUNCU KEDI COZUM:
+
+    public static boolean isCatPlaying (boolean isSummer, int temperature){
+        if(isSummer){
+            return temperature > 25 && temperature <= 45;
+        }
+        return temperature > 25 && temperature <= 35;
+    }
+
+    //FARKLI BIR YOL:
+
+    public static boolean isCatPlaying2 (boolean isSummer, int temperature){
+        int limit = isSummer ? 45 : 35;
+        return temperature > 25 && temperature <= limit;
     }
 }
