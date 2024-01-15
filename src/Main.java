f39afa1cb00ccb20a6eb1a139bab3deb71c44c12
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-15 17:11:40 +0300

    Yas Tespiti methodu tamamlandi

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,14 @@ public class Main {
         System.out.println("Havlayan Kopegimiz: " + shouldWakeUp(false, 2));
         System.out.println("Havlayan Kopegimiz: " + shouldWakeUp(true, 8));
         System.out.println("Havlayan Kopegimiz: " + shouldWakeUp(true, -1));
+
+        System.out.println("Yaş Tespiti: " + hasTeen(9, 99, 19));
+        System.out.println("Yaş Tespiti: " + hasTeen(23, 15, 42));
+        System.out.println("Yaş Tespiti: " + hasTeen(22, 23, 34));
+
+        System.out.println("Yaş Tespiti: " + hasTeen2(9, 99, 19));
+        System.out.println("Yaş Tespiti: " + hasTeen2(23, 15, 42));
+        System.out.println("Yaş Tespiti: " + hasTeen2(22, 23, 34));
     }
 
     // NOT: Metodumuzu statik bir main metodunun icinde cagirabilmek icin statik yazmaliyiz.
@@ -18,5 +26,23 @@ public class Main {
 
         if(clock < 0 || clock > 23 || !isDogBarking) return false;
         return clock < 8 || clock >= 20;
+    }
+
+    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge){
+        return (firstAge >= 13 && firstAge <=19) || (secondAge >= 13 && secondAge <=19) || (thirdAge >= 13 && thirdAge <=19);
+    }
+
+    //FARKLI BIR YOL:
+
+    public static boolean hasTeen2(int... ages){
+        if(ages.length != 3){
+            return false;
+        }
+        for (int age: ages){
+            if(age>=13 && age <=19){
+                return true;
+            }
+        }
+        return false;
     }
 }
